8735082f94025b2541ed8aad1f92f280824a228a
Sushmi27 <sushmitha272005@gmail.com>
2025-06-18 21:24:59 +0530

    Added proxypattern-Main file

---
+++ ProxyPattern/Main.java
@@ -0,0 +1,17 @@
+package ProxyPattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("image1.jpg");
+        Image image2 = new ProxyImage("image2.jpg");
+
+        System.out.println("Accessing image1 for the first time:");
+        image1.display();
+
+        System.out.println("\nAccessing image1 again:");
+        image1.display();
+
+        System.out.println("\nAccessing image2 for the first time:");
+        image2.display();
+    }
+}
