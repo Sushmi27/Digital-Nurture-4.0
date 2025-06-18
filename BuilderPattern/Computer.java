405335170e5e18fceae410ee9079db9641145ec7
Sushmi27 <sushmitha272005@gmail.com>
2025-06-18 20:48:56 +0530

    Added builderpattern-Computer file

---
+++ BuilderPattern/Computer.java
@@ -0,0 +1,57 @@
+package BuilderPattern;
+
+public class Computer {
+
+    private String cpu;
+    private String ram;
+    private String storage;
+
+    private Computer(Builder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.storage = builder.storage;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void showConfig() {
+        System.out.println("CPU: " + cpu);
+        System.out.println("RAM: " + ram);
+        System.out.println("Storage: " + storage);
+    }
+
+    public static class Builder {
+        private String cpu;
+        private String ram;
+        private String storage;
+
+        public Builder setCpu(String cpu) {
+            this.cpu = cpu;
+            return this;
+        }
+
+        public Builder setRam(String ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        public Builder setStorage(String storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
